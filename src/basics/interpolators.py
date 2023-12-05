6c6bc36fa78d9d8f5fe778959cbcb8f30ed42dc4
bwrob <bw2002@wp.pl>
2023-12-05 22:19:11 +0100

    Refactored interpolator functionality.

---
+++ src/basics/interpolators.py
@@ -0,0 +1,131 @@
+"""Interpolator objects.
+
+Interpolator objects are initiated with:
+    x_values - sorted list of numeric values (int, float, np.number)
+    y_values - list of numeric values
+    extrapolate - boolean if object should return value if call is outside range.
+
+Example usage:
+    x_values = [1, 2, 4, ]
+    y_values = [2, 8, 4.5, ]
+    interpolator = LinearInterpolator(
+        x_values,
+        y_values,
+        extrapolate=True
+    )
+    print([interpolator(x) for x in range(8)])
+        >>[2.0, 2.0, 8.0, 6.25, 4.5, 4.5, 4.5, 4.5]
+
+Base abstract class:
+    Interpolator
+Concrete implementations:
+    LinearInterpolator
+"""
+
+from abc import ABC, abstractmethod
+from enum import IntEnum
+from typing import List, Union
+
+import numpy as np
+from attrs import define, field
+
+NumericType = Union[int, float, np.number]
+
+
+class ExtrapolateIndex(IntEnum):
+    """Helper extrapolations enum.
+
+    Indicates if we are in front of or in the back of the interpolation
+    range.
+    """
+
+    FRONT = -1
+    BACK = -2
+
+
+@define(slots=False)
+class Interpolator(ABC):
+    """Abstract base class for interpolator objects."""
+
+    _xs: List[NumericType] = field(alias="x_values")
+    _ys: List[NumericType] = field(alias="y_values")
+    _extrapolate: bool = field(
+        alias="extrapolate",
+        default=False,
+    )
+
+    @_xs.validator
+    def check_x_values(self, attribute, values):  # pylint: disable=W0613
+        """Validates that x_values are sorted."""
+        if not values:
+            raise ValueError("list of x values is empty.")
+        if len(values) == 1:
+            return
+        for i in range(len(values) - 1):
+            if values[i + 1] - values[i] < 0:
+                raise ValueError("List of x values is not sorted")
+
+    @_ys.validator
+    def check_y_values(self, attribute, values):  # pylint: disable=W0613
+        """Validates that y_values equal length to x_values."""
+        if len(values) != len(self._xs):
+            raise ValueError("List of y values is different len from x values.")
+
+    @property
+    def x_values(self) -> List[float]:
+        """Get x values."""
+        return [float(x) for x in self._xs]
+
+    @property
+    def y_values(self) -> List[float]:
+        """Get y values."""
+        return [float(x) for x in self._ys]
+
+    @property
+    def is_extrapolator(self) -> bool:
+        """Check if object extrapolates."""
+        return self._extrapolate
+
+    @abstractmethod
+    def __call__(self, x: NumericType) -> float:
+        """Call to get interpolated y value."""
+
+    def __len__(self):
+        """Get length of interpolator."""
+        # unambiguous since we validated equal len
+        return len(self._xs)
+
+
+class LinearInterpolator(Interpolator):
+    """Interpolator using linear interpolation, constant extrapolation."""
+
+    def __call__(self, x: float) -> float:
+        """Call to get interpolated y value."""
+        index = self.__find_index(x)
+
+        # negative index mean outside range
+        if index < 0 and not self.is_extrapolator:
+            raise ValueError(
+                "Given range outside of interpolated range to non-extrapolator."
+            )
+        match index:
+            case ExtrapolateIndex.FRONT:
+                result = self._ys[0]
+            case ExtrapolateIndex.BACK:
+                result = self._ys[-1]
+            case _:
+                x_delta = self._xs[index + 1] - self._xs[index]
+                y_delta = self._ys[index + 1] - self._ys[index]
+                slope = y_delta / x_delta
+                result = self._ys[index] + (x - self._xs[index]) * slope
+        # enforce float -> flot signature of interpolator
+        return float(result)
+
+    def __find_index(self, x: float) -> int:
+        """Helper function to get the adjacent index."""
+        if x < self._xs[0]:
+            return ExtrapolateIndex.FRONT
+        for i in range(len(self) - 1):
+            if self._xs[i] <= x < self._xs[i + 1]:
+                return i
+        return ExtrapolateIndex.BACK
