4e2ef2ae8493739325526a03819d52d0e8ee3ba1
bwrob <bw2002@wp.pl>
2024-01-20 18:18:13 +0100

    Refactoring to integration submodule

---
+++ optionslib/math/integration/integration_schema.py
@@ -0,0 +1,36 @@
+"""Implementations of the integration schema."""
+
+from typing import Callable
+
+import numpy as np
+
+from optionslib.math.integration.integration_schema_configs import (
+    RectangleConfig,
+    IntegrationConfig,
+)
+from optionslib.types.var_types import NumericType
+
+IntegrationSchema = Callable[
+    [Callable, NumericType, NumericType, IntegrationConfig], NumericType
+]
+
+
+def rectangle_rule(
+    integrand: Callable,
+    start: NumericType,
+    end: NumericType,
+    config: RectangleConfig,
+) -> NumericType:
+    """Implementation of the rectangle rule for integration.
+
+    Args:
+        integrand: function to integrate
+        start: start of integration interval
+        end: end of integration interval
+        config: integration configuration
+    """
+    steps = config.steps
+    x_points = np.linspace(start, end, steps)
+    values = integrand(x_points)
+    dx = (end - start) / np.float64(steps)
+    return np.sum(values) * dx
