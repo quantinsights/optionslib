f44251b7988114384fcd31a99c9e9f86cc137087
bwrob <bw2002@wp.pl>
2024-01-27 19:05:58 +0100

    Update holiday_calendar.py

--- optionslib/time/holiday_calendar.py
+++ optionslib/time/holiday_calendar.py
@@ -67,7 +67,7 @@ class HolidayCalendar:
         https://github.com/OpenGamma/Strata/blob/main/modules/basics/src/main/java/com/opengamma/strata/basics/date/GlobalHolidayCalendars.java
 
         """
-        if self.holiday_calendar_id == HolidayCalendarId.LONDON:
+        if self.holiday_calendar_id is HolidayCalendarId.LONDON:
             self.generate_london_calendar()
 
     def generate_london_calendar(self) -> None:
@@ -80,69 +80,83 @@ class HolidayCalendar:
         holidays = []
 
         for year in range(1950, 2101, 1):
-            # new year
-            if year >= 1974:
-                holidays.append(time_utils.bump_to_mon(dt.date(year, 1, 1)))
+            self.__append_new_year(holidays, year)
+            self.__append_easter_holidays(holidays, year)
+            self.__append_early_may(holidays, year)
+            self.__append_spring_holidays(holidays, year)
+            self.__append_summer_holidays(holidays, year)
+            self.__append_christmas(holidays, year)
 
-            # easter
-            holidays.append(time_utils.easter(year) - dt.timedelta(days=2))
-            holidays.append(time_utils.easter(year) + dt.timedelta(days=1))
-
-            # early may
-            if year in [1995, 2020]:
-                holidays.append(dt.date(year, 5, 8))
-            else:
-                if year >= 1978:
-                    holidays.append(
-                        time_utils.first_in_month(year, 5, DayOfWeek.MONDAY)
-                    )
-
-            # spring
-            if year == 2002:
-                # golden jubilee
-                holidays.append(dt.date(2002, 6, 3))
-                holidays.append(dt.date(2002, 6, 4))
-            elif year == 2012:
-                # diamond jubilee
-                holidays.append(dt.date(2012, 6, 4))
-                holidays.append(dt.date(2012, 6, 5))
-            elif year == 2022:
-                # platinum jubilee
-                holidays.append(dt.date(2022, 6, 2))
-                holidays.append(dt.date(2022, 6, 3))
-            elif year in [1967, 1970]:
-                holidays.append(time_utils.last_in_month(year, 5, DayOfWeek.MONDAY))
-            elif year < 1971:
-                # White sunday
-                holidays.append(time_utils.easter(year) + dt.timedelta(days=50))
-            else:
-                holidays.append(time_utils.last_in_month(year, 5, DayOfWeek.MONDAY))
-
-            # summer
-            if year < 1965:
-                holidays.append(time_utils.first_in_month(year, 8, DayOfWeek.MONDAY))
-            elif year < 1971:
-                holidays.append(
-                    time_utils.last_in_month(year, 8, DayOfWeek.SATURDAY)
-                    + dt.timedelta(days=2)
-                )
-            else:
-                holidays.append(time_utils.last_in_month(year, 8, DayOfWeek.MONDAY))
-
-            # queen's funeral
-            if year == 2022:
-                holidays.append(dt.date(2022, 9, 19))
-
-            # Christmas
-            holidays.append(time_utils.christmas_bumped_sat_or_sun(year))
-            holidays.append(time_utils.boxing_day_bumped_sat_sun(year))
-
-        holidays.append(dt.date(1999, 12, 31))  # millenium
+        holidays.append(dt.date(1999, 12, 31))  # millennium
         holidays.append(dt.date(2011, 4, 29))  # royal wedding
+        holidays.append(dt.date(2022, 9, 19))  # queen's funeral
         holidays.append(dt.date(2023, 5, 8))  # king's coronation
 
         holidays = self.remove_sat_sun(holidays)
         self.__holiday_dates = holidays
+
+    @staticmethod
+    def __append_christmas(holidays, year):
+        """Append christmas."""
+        holidays.append(time_utils.christmas_bumped_sat_or_sun(year))
+        holidays.append(time_utils.boxing_day_bumped_sat_sun(year))
+
+    @staticmethod
+    def __append_easter_holidays(holidays, year):
+        """Appends easter holidays."""
+        holidays.append(time_utils.easter(year) - dt.timedelta(days=2))
+        holidays.append(time_utils.easter(year) + dt.timedelta(days=1))
+
+    @staticmethod
+    def __append_new_year(holidays, year):
+        """Append new year."""
+        if year >= 1974:
+            holidays.append(time_utils.bump_to_mon(dt.date(year, 1, 1)))
+
+    @staticmethod
+    def __append_summer_holidays(holidays, year):
+        """Append summer holidays."""
+        if year < 1965:
+            holidays.append(time_utils.first_in_month(year, 8, DayOfWeek.MONDAY))
+        elif year < 1971:
+            holidays.append(
+                time_utils.last_in_month(year, 8, DayOfWeek.SATURDAY)
+                + dt.timedelta(days=2)
+            )
+        else:
+            holidays.append(time_utils.last_in_month(year, 8, DayOfWeek.MONDAY))
+
+    @staticmethod
+    def __append_spring_holidays(holidays, year):
+        """Append spring holidays."""
+        if year == 2002:
+            # golden jubilee
+            holidays.append(dt.date(2002, 6, 3))
+            holidays.append(dt.date(2002, 6, 4))
+        elif year == 2012:
+            # diamond jubilee
+            holidays.append(dt.date(2012, 6, 4))
+            holidays.append(dt.date(2012, 6, 5))
+        elif year == 2022:
+            # platinum jubilee
+            holidays.append(dt.date(2022, 6, 2))
+            holidays.append(dt.date(2022, 6, 3))
+        elif year in [1967, 1970]:
+            holidays.append(time_utils.last_in_month(year, 5, DayOfWeek.MONDAY))
+        elif year < 1971:
+            # White sunday
+            holidays.append(time_utils.easter(year) + dt.timedelta(days=50))
+        else:
+            holidays.append(time_utils.last_in_month(year, 5, DayOfWeek.MONDAY))
+
+    @staticmethod
+    def __append_early_may(holidays, year):
+        """Appends early may holidays."""
+        if year in [1995, 2020]:
+            holidays.append(dt.date(year, 5, 8))
+        else:
+            if year >= 1978:
+                holidays.append(time_utils.first_in_month(year, 5, DayOfWeek.MONDAY))
 
     def remove_sat_sun(self, holidays: List[dt.date]) -> List[dt.date]:
         """Removes the first weekend day and second weekend day from the list of
